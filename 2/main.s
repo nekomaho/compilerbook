2e6f2dc0cd5ecceb36a26fa17a722e01de33fe5e
nekomaho <nekosatoru@gmail.com>
2018-11-28 21:02:26 +0900

    Add function call

---
+++ 2/main.s
@@ -0,0 +1,43 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 14
+	.globl	_plus                   ## -- Begin function plus
+	.p2align	4, 0x90
+_plus:                                  ## @plus
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %esi
+	addl	-8(%rbp), %esi
+	movl	%esi, %eax
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$1, %edi
+	movl	$2, %esi
+	movl	$0, -4(%rbp)
+	callq	_plus
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+
+.subsections_via_symbols
